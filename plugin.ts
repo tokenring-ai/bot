478a868da8f0b75a0679553ad4d6886fae9a42a0
Mark Dierolf <mdierolf@financialcontent.com>
2026-07-24 16:34:19 -0700

    Switch plugin `config` to `configSchema`, refactor configuration handling

--- plugin.ts
+++ plugin.ts
@@ -28,5 +28,5 @@ export default {
       app.waitForService(AgentCommandService, agentCommandService => agentCommandService.addAgentCommands(agentCommands));
     }
   },
-  config: packageConfigSchema,
+  configSchema: packageConfigSchema,
 } satisfies TokenRingPlugin<typeof packageConfigSchema>;
